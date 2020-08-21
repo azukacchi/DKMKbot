8210f1d728516bdabf1f39f3cab96682ff16fcd0
azukacchi <azukacchi@gmail.com>
2020-08-22 04:22:12 +0700

    modified createtable.py

--- createtable.py
+++ createtable.py
@@ -3,9 +3,7 @@ import os.path
 from main import createtable
 import config
 
-auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
-auth.set_access_token(config.access_token, config.access_token_secret)
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
 filename = 'yourdatabasefile.db'
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
